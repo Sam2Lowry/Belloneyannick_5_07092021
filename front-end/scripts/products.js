043fbc8b1c9198346fbfe17e2ee453b5cda7eb91
Sam2Lowry <yannick.bellone@outlook.fr>
2021-09-24 15:07:27 +0200

    random reviewing

--- front-end/scripts/products.js
+++ front-end/scripts/products.js
@@ -5,40 +5,48 @@ const stockCameras = "http://localhost:3000/api/cameras";
 // lenses, _id, name, price, description, imageUrl
 // Constante intersectionnelles : cartItems, cart, queryID on links
 
+// Fonction mathématique pour les avis
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+let star = '<i class="bi bi-star-fill text-dark mx-1"></i>';
+
 //Promesse d'obtention des data depuis l'api
+
 fetch(stockCameras).then((response) =>
   response.json().then((data) => {
     console.log(data);
     // Définition du container des "cards" des produits
     const container = document.getElementById("productsStack");
     data.forEach((item) => {
-     
-      //Construction de la carte
+
+    //Construction de la carte
       const content = `
       <div class="col-md-6 col-lg-3">
-          <div class="card bg-light">
-        <div class="card-body text-center">
-        <img
-        src="${item.imageUrl}"
-        class="rounded-circle mb-3 img-fluid"
-        alt=""
+      <div class="card bg-light">
+      <div class="card-body text-center">
+      <img
+      src="${item.imageUrl}"
+      class="rounded-circle mb-3 img-fluid"
+      alt=""
       />
       <h3 class="card-title mb-3">${item.name}</h3>
       <p class="card-text">${item.description}</p>
-      <a href="#"><i class="bi bi-star-fill text-dark mx-1"></i></a>
-      <a href="#"><i class="bi bi-star-fill text-dark mx-1"></i></a>
-      <a href="#"><i class="bi bi-star-fill text-dark mx-1"></i></a>
-      <a href="#"><i class="bi bi-star-fill text-dark mx-1"></i></a>
-      <a href="#"><i class="bi bi-star-half text-dark mx-1"></i></a>
+      ${star.repeat(getRandomInt(1,6))}
       <div>
-        <button class="btn-dark mt-3">Voir le produit</button>
-        </div>
-        </div>
-        </div>
-        </div>
+      <button class="btn-dark mt-3">Voir le produit</button>
+      </div>
+      </div>
+      </div>
+      </div>
             `;
 
+    //injection dans le html de celle ci au sein de la section produit
       container.innerHTML += content;
+
     });
   })
+  .catch(err => alert(err))
 );
