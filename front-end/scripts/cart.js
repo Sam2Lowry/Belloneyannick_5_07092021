2eb8355ca1ee407f1c0f70dcdf12257762f43886
Sam2Lowry <yannick.bellone@outlook.fr>
2021-10-04 13:23:18 +0200

    page deployment cart

--- front-end/scripts/cart.js
+++ front-end/scripts/cart.js
@@ -7,16 +7,12 @@ document.addEventListener("DOMContentLoaded", cartToken);
 function cartToken() {
   var cartCounter = 0;
   cartCounter = localStorage.length;
-  
+  console.log(cartCounter);
   if (cartCounter === 0) {
-    document.getElementById(
-      "tokenCount"
-    ).textContent = "Panier vide";
+    document.getElementById("tokenCount").textContent = "Panier vide";
   } else {
     document.getElementById(
       "tokenCount"
     ).textContent = `Panier (${cartCounter})`;
   }
 }
-
-
