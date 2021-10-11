c51f4c5880763ad663a80c2cbffc880f4ae5b3d9
Sam2Lowry <yannick.bellone@outlook.fr>
2021-10-11 15:57:55 +0200

    fix total

--- front-end/scripts/cart.js
+++ front-end/scripts/cart.js
@@ -51,6 +51,7 @@ function updateCartTotal() {
   //Prix hors boucle
   console.log(totalPrice);
   document.getElementById("totalPrice").textContent = `Prix total : ${totalPrice}`;
+  
 }
 
 
@@ -110,14 +111,14 @@ function loadCart() {
   <path d="M5.929 1.757a.5.5 0 1 0-.858-.514L2.217 6H.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h.623l1.844 6.456A.75.75 0 0 0 3.69 15h8.622a.75.75 0 0 0 .722-.544L14.877 8h.623a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1.717L10.93 1.243a.5.5 0 1 0-.858.514L12.617 6H3.383L5.93 1.757zM4 10a1 1 0 0 1 2 0v2a1 1 0 1 1-2 0v-2zm3 0a1 1 0 0 1 2 0v2a1 1 0 1 1-2 0v-2zm4-1a1 1 0 0 1 1 1v2a1 1 0 1 1-2 0v-2a1 1 0 0 1 1-1z"></path>
 </svg>
               </span>
-              <input type="number" class="form-control" placeholder="${
+              <input type="number" class="form-control" placeholder="0" value="${
                 item.quantity
               }" min="1" max="100" aria-label="quantité">
             </div>
       </div>
       <div class="d-flex flex-row">
-        <h5 class="text-grey mx-auto price-tag">${
-          formatter.format (item.price * item.quantity)
+        <h5 class="text-grey mx-auto price-tag">${ 
+          item.price * item.quantity
         }</h5>
       </div>
       <div class="d-flex align-items-center btn-supp">
