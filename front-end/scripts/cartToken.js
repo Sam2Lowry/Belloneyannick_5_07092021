6ba3d893ec9001ea1d57b1c6718d41a293f105f7
Sam2Lowry <yannick.bellone@outlook.fr>
2021-10-11 16:32:41 +0200

    cart cross pages ok

---
+++ front-end/scripts/cartToken.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", cartToken);
+function cartToken() {
+  var cartCounter = 0;
+  cartCounter = localStorage.length;
+  console.log(cartCounter);
+  if (cartCounter === 0) {
+    document.getElementById("tokenCount").textContent = "Panier vide";
+  } else {
+    document.getElementById(
+      "tokenCount"
+    ).textContent = `Panier (${cartCounter})`;
+  }
+}
+
